27108a04d999d54c6b2e1c5934c061fb132bf107
Unknown <jameshu1205@gmail.com>
2018-04-28 11:00:19 +0800

    排版並初始化陣列(城市已可成功執行)

--- 2018$4$26$HOMEWORK.cpp
+++ 2018$4$26$HOMEWORK.cpp
@@ -1,67 +1,33 @@
-#include <iostream>
-
+﻿#include <iostream>
 using namespace std;
-
 int main() {
-
     int products;
-
-        cout << "請輸入買了幾樣商品";
-
+        cout << "請輸入買了幾樣商品\n";
         cin >> products;
-
-    
-
-    int price[products];
-
+    int price[products] = {0};
     for (int i = 0; i < products; i++){
-
-        cout <<"請輸入第" << i + 1 << "個商品金額";
-
+        cout <<"請輸入第" << i + 1 << "個商品金額\n";
         cin >> price[i];
-
         price[i] %= 100;    
-
     }
-
     int allCost = 0;
-
     for (int i = 0; i < products; i++){
-
         allCost += price[i];
-
     }
-
     int biggestPrice = 0;
-
     for (int i = 0; i < products;i++){
-
         if (price[i] > allCost/2){
-
             biggestPrice = price[i];
-
         }else{
-
             int i = 0;
-
             for (int j = 0; j < products; j++){
-
                 if (price[j] > i){
-
                     i = price[j];
-
                 }
-
             }
-
             biggestPrice = i;
-
         }
-
     }
-
     cout << "第一位找零" << 100 - biggestPrice%100 << "元\n";
-
     cout << "第二位找零" << 100 - (allCost - biggestPrice)%100 << "元\n";
-
 }
